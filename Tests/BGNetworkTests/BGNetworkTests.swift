458aef8b9f21abafc5a38d689f2605304defde08
Nilesh <nileshcse14@gmail.com>
2023-12-12 18:10:44 +0000

    Initial Commit

---
+++ Tests/BGNetworkTests/BGNetworkTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import BGNetwork
+
+final class BGNetworkTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
